e90e4095a579ae58ec9f93cae29f86dd89eeacbc
sethuvishal <sethuvishals@gmail.com>
2026-06-26 01:48:54 +0530

    add borrow_key_from_page function definition in btree.h file

--- btree/btree.h
+++ btree/btree.h
@@ -16,3 +16,6 @@ void split_node(Btree* btree, Page* page, Page* parent_page);
 int find(Btree* btree, int search_key, Page* page);
 bool update(Btree* btree, int update_key, int new_value, Page* page);
 bool delete(Btree* btree, int delete_key, Page* page, Page* parent_page);
+
+bool borrow_from_left_page(Btree* btree, Page* parent_page, Page* current_page);
+bool borrow_from_right_page(Btree* btree, Page* parent_page, Page* current_page);
